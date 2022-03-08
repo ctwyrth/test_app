c6706d05fe1b46d213c40bb93d8a66c00d51dc1a
C. Thomas Wyrth <mmxvi.ctw@gmail.com>
2022-03-08 15:01:26 -0800

    Second commit - person cards.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 
+import MyNewComponent from './components/MyNewComponent';
+import PersonCard from './components/PersonCard';
+
 function App() {
   return (
-    <div>
-      <h1>Hello Dojo!</h1>
-      <h3>Things I need to do:</h3>
-      <ul>
-        <li>Learn React</li>
-        <li>Climb Mt. Everest</li>
-        <li>Run a marathon</li>
-        <li>Feed the dogs</li>
-      </ul>
+    <div className='App'>
+      <MyNewComponent someText="Hello World!" />
+      <MyNewComponent someText="I am reusing this component." />
+      <br />
+      <div className='App-card'>
+        <PersonCard lastName="Doe" firstName="Jane" age={45} hairColor="Black" />
+        <PersonCard lastName="Smith" firstName="John" age={88} hairColor="Brown" />
+        <PersonCard lastName="Fillmore" firstName="Millard" age={50} hairColor="Brown" />
+        <PersonCard lastName="Smith" firstName="Maria" age={62} hairColor="Blonde" />
+      </div>
     </div>
   );
 }
